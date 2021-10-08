df8fff2f31b2e9b8eabe9bbe8571e8653ccbe327
Daniel Olakotan <horlakz@protonmail.com>
2021-10-08 22:10:53 +0100

    fixed a lot errors

--- assets/Typescript/script.ts
+++ assets/Typescript/script.ts
@@ -1,68 +1,101 @@
 // export {};
 
+// ASSIGNING SCORES TO VALUES
 const grades: any = {
-	A1: 4,
-	B2: 3.6,
-	B3: 3.2,
-	C4: 2.8,
-	C5: 2.4,
-	C6: 2
+  A1: 4,
+  B2: 3.6,
+  B3: 3.2,
+  C4: 2.8,
+  C5: 2.4,
+  C6: 2,
 };
 
-const otherSubjectsGrades: string[] = ["other-subjects-grade1", "other-subjects-grade2", "other-subjects-grade3"];
+// id OF OTHER SUBJECTS
+const otherSubjectsGrades: string[] = [
+  "other-subjects-grade1",
+  "other-subjects-grade2",
+  "other-subjects-grade3",
+];
 
-const coursesIds = ["mathematics-grade", "english-grade", ...otherSubjectsGrades];
+// ALL O-LEVEL IDs
+const coursesIds: string[] = [
+  "mathematics-grade",
+  "english-grade",
+  ...otherSubjectsGrades,
+];
 
-function sum(...values:number[]) : number {
-	var total = 0;
+// ADDING ALL THE SCORES TOGETHER
+function sum(...values: number[]): number {
+  var total = 0;
 
-	for (const value of values) {
-		total += value;
-	}
+  for (const value of values) {
+    total += value;
+  }
 
-	return total;
+  return total;
 }
 
-function to_int(value: string):number {
-    return Number.parseInt(value);
-  }
-  
-  function get_grade_point(grade:any):number {
-    return grades[grade];
-  }
-  
-  function display(value:number) {
-    var display_container:any = document.querySelector("#message");
-    var display_board : any = document.querySelector("#result");
-    var display_button : any = document.querySelector("#calculate");
-    const noScore = document.querySelector('#no-score');
-    const resetValues = document.getElementById("reset-values");
-  
-    display_container.style!.display = "block";
-  
-    if (value) {
-      display_board.innerText = value;
-      display_button!.innerText = "Calculate again";
-      // resetValues.click()
-    } 
-  
-  function calculate_point() {
-    const courses_grade_points = [];
-    for (const course of coursesIds) {
-      var grade:number = parseInt((document.querySelector(`#${course}`) as HTMLInputElement).value, 10);
-      courses_grade_points.push(get_grade_point(grade));
-    }
-    
-    const [mathematics, english, otherSubjects1, otherSubjects2, otherSubjects3] = courses_grade_points;
-  
+// PARSING THE JAMB SCORE AND POST-UTME SCORE VALUES INTO A NUMBER
+function to_int(value: string): number {
+  return Number.parseInt(value);
+}
 
-    let grade_scores:number, jambscore:number, postutmescore:number;
-  
-    grade_scores = sum(mathematics, english, otherSubjects1, otherSubjects2, otherSubjects3);
-    jambscore = to_int((<HTMLInputElement>document.querySelector(`#jambscore`)).value) / 8;
-    postutmescore = to_int((<HTMLInputElement>document.querySelector(`#postutmescore`)).value);
-  
-    var total_score:number = sum(grade_scores, jambscore, postutmescore);
-    display(total_score);
+// CONVERTING THE OPTION VALUE INTO SCORE VALUE
+function get_grade_point(grade: string): number {
+  return grades[grade];
+}
+
+// DISPLAYING THE RESULT FUNCTION
+function display(value: number) {
+  var display_container: any = document.querySelector("#message"); // THE RESULT MESSAGE
+  var display_board: any = document.querySelector("#result"); // THE RESULT BLOCK
+  var display_button: any = document.querySelector("#calculate"); // CALCULATE BUTTON
+
+  display_container.style.display = "block";
+
+  if (value) {
+    display_board.innerText = value;
+    display_button!.innerText = "Calculate again";
+    // resetValues.click() NOT NECESSARY
   }
+}
+
+// CALCULATOR FUNCTION
+function calculate_point() {
+  const courses_grade_points = [];
+  for (const valueResult of coursesIds) {
+    var grade: string = (
+      document.querySelector(`#${valueResult}`) as HTMLInputElement
+    ).value;
+    courses_grade_points.push(get_grade_point(grade));
+  } // LOOPING THROUGH THE O-LEVEL IDS TO GET THE VALUES
+
+  const [
+    mathematics,
+    english,
+    otherSubjects1,
+    otherSubjects2,
+    otherSubjects3,
+  ]: number[] = courses_grade_points; // GETTING THE O-LEVEL VALUES
+
+  let grade_scores: number, jambscore: number, postutmescore: number;
+
+  grade_scores = sum(
+    mathematics,
+    english,
+    otherSubjects1,
+    otherSubjects2,
+    otherSubjects3
+  ); // ADDING THE O-LEVEL VALUES
+
+  jambscore =
+    to_int((<HTMLInputElement>document.querySelector(`#jambscore`)).value) / 8; // JAMBSCORE
+
+  postutmescore = to_int(
+    (<HTMLInputElement>document.querySelector(`#postutmescore`)).value // POST-UTME SCORE
+  );
+
+  var total_score: number = sum(grade_scores, jambscore, postutmescore); // ADDING THE TOTAL SCORES
+ 
+  display(total_score); // RUNNING THE DISPLAY FUNCTION
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,27 @@
 "use strict";
 // export {};
+// ASSIGNING SCORES TO VALUES
 const grades = {
     A1: 4,
     B2: 3.6,
     B3: 3.2,
     C4: 2.8,
     C5: 2.4,
-    C6: 2
+    C6: 2,
 };
-const otherSubjectsGrades = ["other-subjects-grade1", "other-subjects-grade2", "other-subjects-grade3"];
-const coursesIds = ["mathematics-grade", "english-grade", ...otherSubjectsGrades];
+// id OF OTHER SUBJECTS
+const otherSubjectsGrades = [
+    "other-subjects-grade1",
+    "other-subjects-grade2",
+    "other-subjects-grade3",
+];
+// ALL O-LEVEL IDs
+const coursesIds = [
+    "mathematics-grade",
+    "english-grade",
+    ...otherSubjectsGrades,
+];
+// ADDING ALL THE SCORES TOGETHER
 function sum(...values) {
     var total = 0;
     for (const value of values) {
@@ -17,36 +29,40 @@ function sum(...values) {
     }
     return total;
 }
+// PARSING THE JAMB SCORE AND POST-UTME SCORE VALUES INTO A NUMBER
 function to_int(value) {
     return Number.parseInt(value);
 }
+// CONVERTING THE OPTION VALUE INTO SCORE VALUE
 function get_grade_point(grade) {
     return grades[grade];
 }
+// DISPLAYING THE RESULT FUNCTION
 function display(value) {
-    var display_container = document.querySelector("#message");
-    var display_board = document.querySelector("#result");
-    var display_button = document.querySelector("#calculate");
-    const noScore = document.querySelector('#no-score');
-    const resetValues = document.getElementById("reset-values");
+    var display_container = document.querySelector("#message"); // THE RESULT MESSAGE
+    var display_board = document.querySelector("#result"); // THE RESULT BLOCK
+    var display_button = document.querySelector("#calculate"); // CALCULATE BUTTON
     display_container.style.display = "block";
     if (value) {
         display_board.innerText = value;
         display_button.innerText = "Calculate again";
-        // resetValues.click()
+        // resetValues.click() NOT NECESSARY
     }
-    function calculate_point() {
-        const courses_grade_points = [];
-        for (const course of coursesIds) {
-            var grade = parseInt(document.querySelector(`#${course}`).value, 10);
-            courses_grade_points.push(get_grade_point(grade));
-        }
-        const [mathematics, english, otherSubjects1, otherSubjects2, otherSubjects3] = courses_grade_points;
-        let grade_scores, jambscore, postutmescore;
-        grade_scores = sum(mathematics, english, otherSubjects1, otherSubjects2, otherSubjects3);
-        jambscore = to_int(document.querySelector(`#jambscore`).value) / 8;
-        postutmescore = to_int(document.querySelector(`#postutmescore`).value);
-        var total_score = sum(grade_scores, jambscore, postutmescore);
-        display(total_score);
-    }
+}
+// CALCULATOR FUNCTION
+function calculate_point() {
+    const courses_grade_points = [];
+    for (const valueResult of coursesIds) {
+        var grade = document.querySelector(`#${valueResult}`).value;
+        courses_grade_points.push(get_grade_point(grade));
+    } // LOOPING THROUGH THE O-LEVEL IDS TO GET THE VALUES
+    const [mathematics, english, otherSubjects1, otherSubjects2, otherSubjects3,] = courses_grade_points; // GETTING THE O-LEVEL VALUES
+    let grade_scores, jambscore, postutmescore;
+    grade_scores = sum(mathematics, english, otherSubjects1, otherSubjects2, otherSubjects3); // ADDING THE O-LEVEL VALUES
+    jambscore =
+        to_int(document.querySelector(`#jambscore`).value) / 8; // JAMBSCORE
+    postutmescore = to_int(document.querySelector(`#postutmescore`).value // POST-UTME SCORE
+    );
+    var total_score = sum(grade_scores, jambscore, postutmescore); // ADDING THE TOTAL SCORES
+    display(total_score); // RUNNING THE DISPLAY FUNCTION
 }
